fa805ca1b837c306d0d296551c193adc2b3cba70
basma <basmaashouur@gmail.com>
2018-02-09 22:06:14 +0200

    Added Bootsrap Animated Sidenav

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var map;
      // Function to initialize the map within the map div
-     function initMap() {
+function initMap() {
        map = new google.maps.Map(document.getElementById('map'), {
          center: {lat: 40.74135, lng: -73.99802},
          zoom: 14
@@ -8,3 +8,12 @@ var map;
        // Create a single latLng literal object.
        var singleLatLng = {lat: 40.74135, lng: -73.99802};
    }
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
